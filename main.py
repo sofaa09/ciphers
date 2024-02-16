ad593204e371527d63eb2ae27d39264044044582
sofa <136380277+sofaa09@users.noreply.github.com>
2024-02-16 17:44:12 +0100

    main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import interface
 from ciphers import caesar_encrypt, atbash_encrypt
 def encrypt():
     currentcipher = interface.choice.get()
-    if currentcipher == 'Цезарь':   
+    if currentcipher == 'caesar':   
         plaintext = interface.plaintextbox.get()
         key = int(interface.keybox.get())       
         interface.ciphermessage['text'] = caesar_encrypt(plaintext, key)
-    elif currentcipher == 'Атбаш':
+    elif currentcipher == 'atbash':
         plaintext = interface.plaintextbox.get()
         interface.ciphermessage['text'] = atbash_encrypt(plaintext)
 
@@ -19,16 +19,16 @@ def change_cipher():
         #interface.ciphername = 'Цезарь'
     #elif currentcipher == 'Атбаш':
         #interface.ciphername = 'Атбаш'
-    if currentcipher == 'Цезарь':
-        interface.ciphername['text'] = 'Зашифровано шифром Цезаря'
+    if currentcipher == 'caesar':
+        interface.ciphername['text'] = 'encrypted with Caesars cipher'
         interface.keybox['state']='normal'
-    elif currentcipher == 'Атбаш':
-        interface.ciphername['text'] = 'Зашифровано шифром Атбаш'
+    elif currentcipher == 'atbash':
+        interface.ciphername['text'] = 'encrypted with Atbash cipher'
         interface.keybox['state']='disabled'
 
 interface.radiocaesar.config(command=change_cipher)
 interface.radioatbash.config(command=change_cipher)
-interface.choice.set('Атбаш')
+interface.choice.set('atbash')
 change_cipher()
 
 interface.window.mainloop()
